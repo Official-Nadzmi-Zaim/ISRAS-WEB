400c3f4ceea0215eab06911e51a3849fc9c37ef5
Zaim Omar <officialzaim@gmail.com>
2018-04-08 13:38:00 +0800

    Add fake data . Please use database seeder command . Command :  php artisan db:seed

--- app/isras/database/factories/UserFactory.php
+++ app/isras/database/factories/UserFactory.php
@@ -13,11 +13,182 @@ use Faker\Generator as Faker;
 |
 */
 
+$factory->define(App\address::class, function (Faker $faker) {
+    return [
+        'company_id' => $faker->randomNumber($nbDigits = NULL, $strict = false),
+        'addr_1' => $faker->address,
+        'addr_2' => "",
+        'city' => $faker->city,
+        'postcode' => $faker->postcode,
+        'country' => $faker->country,
+    ];
+});
+
+$factory->define(App\admin_blog_content::class, function (Faker $faker) {
+    return [
+        'admin_id' => $faker->randomDigit,
+        'assessment_question_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+    ];
+});
+
+$factory->define(App\admin_feedback_question::class, function (Faker $faker) {
+    return [
+        'admin_id' => $faker->randomDigit,
+        'feedback_question_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+    ];
+});
+
+$factory->define(App\admin_library_content::class, function (Faker $faker) {
+    return [
+        'admin_id' => $faker->randomDigit,
+        'library_content_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+    ];
+});
+
+$factory->define(App\assessment_question::class, function (Faker $faker) {
+    return [
+        'statement' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'type' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'category' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'key_area' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'title' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+    ];
+});
+
+$factory->define(App\assessment_result::class, function (Faker $faker) {
+    return [
+        'result' => $faker->numberBetween($min = 1, $max = 100),
+    ];
+});
+
+$factory->define(App\assessment::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'assessment_question_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'assessment_result_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'score' => $faker->randomFloat($nbMaxDecimals = 2, $min = 1, $max = 100),
+    ];
+});
+
+$factory->define(App\blog_content::class, function (Faker $faker) {
+    return [
+        'title' => $faker->name,
+        'description' => $faker->text,
+    ];
+});
+
+$factory->define(App\company::class, function (Faker $faker) {
+    return [
+        'name' => $faker->company,
+        'ref_no' => $faker->word.$faker->randomNumber($nbDigits = NULL, $strict = false),//sentence($nbWords = 6, $variableNbWords = true),
+    ];
+});
+
+$factory->define(App\feedback_question::class, function (Faker $faker) {
+    return [
+        'description' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+    ];
+});
+
+$factory->define(App\feedback::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'feedback_question_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'score' => $faker->randomFloat($nbMaxDecimals = 2, $min = 1, $max = 100),
+    ];
+});
+
+$factory->define(App\library_content::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence($nbWords = 2, $variableNbWords = true),
+        'description' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'src' => $faker->url,
+        'publication' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'author' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+    ];
+});
+
+$factory->define(App\lookup_assessment_category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(App\lookup_assessment_key_area::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(App\lookup_assessment_title::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(App\lookup_assessment_type::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(App\lookup_author::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(App\lookup_bank::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(App\lookup_country::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(App\lookup_payment_method::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(App\lookup_publication::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+    ];
+});
+
+$factory->define(App\payment::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'company_id' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'amount' => $faker->randomFloat($nbMaxDecimals = 2, $min = 1, $max = 10000),
+        'method' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+        'bank' => $faker->unique()->numberBetween($min = 1, $max = 9000),
+    ];
+});
+
+$factory->define(App\pic::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+    ];
+});
+
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'tel_no' => $faker->unique()->e164PhoneNumber,
+        'fax_no' => $faker->unique()->e164PhoneNumber,
+        'password' => str_random(15),
     ];
 });
+
+//$faker->unique()->safeEmail,
+        //'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        //'remember_token' => str_random(10),
--- app/isras/database/seeds/DatabaseSeeder.php
+++ app/isras/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //1 . factory(App\blog_content::class, 10)->create();
+        //2 . factory(App\company::class, 10)->create();
+        //3 . factory(App\address::class, 10)->create();
+        //4 . factory(App\pic::class, 10)->create();
+        //5 . factory(App\admin_blog_content::class, 10)->create();
+        //6 . factory(App\admin_feedback_question::class, 10)->create();
+        //7 . factory(App\admin_library_content::class, 10)->create();
+        //8 . factory(App\assessment_question::class, 10)->create();
+        //9 . factory(App\assessment_result::class, 10)->create();
+        //10 . factory(App\assessment::class, 10)->create();
+        //11 . factory(App\feedback_question::class, 10)->create();
+        //12 . factory(App\feedback::class, 10)->create();
+        //13 . factory(App\library_content::class, 10)->create();
+        //14 . factory(App\lookup_assessment_category::class, 10)->create();
+        //15 . factory(App\lookup_assessment_key_area::class, 10)->create();
+        //16 . factory(App\lookup_assessment_title::class, 10)->create();
+        //17 . factory(App\lookup_assessment_type::class, 10)->create();
+        //18 . factory(App\lookup_author::class, 10)->create();
+        //19 . factory(App\lookup_bank::class, 10)->create();
+        //20 . factory(App\lookup_country::class, 10)->create();
+        //21 . factory(App\lookup_payment_method::class, 10)->create();
+        //22 . factory(App\lookup_publication::class, 10)->create();
+        //23 . factory(App\payment::class, 10)->create();
+        factory(App\User::class, 10)->create();
     }
 }

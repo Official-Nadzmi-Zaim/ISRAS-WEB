7a0a4a5007af83dc9e09fd02c54b6b5d3698bb50
seladanghijau <nadzmiidzham@gmail.com>
2018-04-15 14:47:06 +0800

    updated BlogController

--- app/isras/app/Http/Controllers/BlogController.php
+++ app/isras/app/Http/Controllers/BlogController.php
@@ -15,26 +15,29 @@ class BlogController extends Controller
 
     public function verifyNewContent(Request $request) {
         $adminId = Auth::user()->id;
-        $blogTitle = $request['blog_title'];
-        $blogDesc = $request['blog_desc'];
 
         if($blogTitle != null || $blogDesc != null) {
-            $newBlogContent = new BlogContent();
-            $newBlogContent->title = $blogTitle;
-            $newBlogContent->description = $blogDesc;
-            $newBlogContent->save();
-
-            $newAdminBlogContent = new AdminBlogContent();
-            $newAdminBlogContent->admin_id = $adminId;
-            $newAdminBlogContent->blog_content_id = $newBlogContent->id;
-            $newAdminBlogContent->save();
-
-            return $newAdminBlogContent;
+            return $this->saveNewContent($adminId, $request);
         } else
             return "Ada benda salah";
     }
 
-    private function saveNewContent() {}
+    private function saveNewContent($adminId, $requestData) {
+        $blogTitle = $requestData['blog_title'];
+        $blogDesc = $requestData['blog_desc'];
+
+        $newBlogContent = new BlogContent();
+        $newBlogContent->title = $blogTitle;
+        $newBlogContent->description = $blogDesc;
+        $newBlogContent->save();
+
+        $newAdminBlogContent = new AdminBlogContent();
+        $newAdminBlogContent->admin_id = $adminId;
+        $newAdminBlogContent->blog_content_id = $newBlogContent->id;
+        $newAdminBlogContent->save();
+
+        return $newAdminBlogContent;
+    }
 
     public function loadUpdateContentForm() {
         return view('pages.admin.content.blog.update');
@@ -42,5 +45,21 @@ class BlogController extends Controller
 
     public function verifyUpdatedContent(Request $request) {}
 
-    private function saveUpdatedContent() {}
+    private function saveUpdatedContent($adminId, $blogId, $requestData) {
+        /*
+        $adminBlogContent = ; // get instance of admin blog content
+        $blogTitle = $requestData['blog_title'];
+        $blogDesc = $requestData['blog_desc'];
+
+        $updatedBlog = BlogContent::find($blogId);
+        $updatedBlog->title = $blogTitle;
+        $updatedBlog->description = $blogDesc;
+        $updatedBlog->save();
+
+        $updatedAdminBlogContent = ; // get id of admin blog content instance
+        $updatedAdminBlogContent->admin_id = $adminId;
+        $updatedAdminBlogContent->blog_content_id = $newBlogContent->id;
+        $updatedAdminBlogContent->save();
+        */
+    }
 }

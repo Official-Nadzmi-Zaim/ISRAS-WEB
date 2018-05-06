6ec73a4e79ecb1bf7a936087d09f1544a5f2fb3c
Zaim Omar <officialzaim@gmail.com>
2018-05-07 00:54:28 +0800

    charts

--- app/isras/config/app.php
+++ app/isras/config/app.php
@@ -180,6 +180,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
 
         Barryvdh\DomPDF\ServiceProvider::class,
+        ConsoleTVs\Charts\ChartsServiceProvider::class,
     ],
 
     /*
@@ -231,6 +232,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        'Charts' => ConsoleTVs\Charts\Facades\Charts::class,
     ],
 
 ];
